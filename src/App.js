ff1fbcd7750c496c2d95737692f7dfa04a223a54
Govardhan Narasimha Murthy <ngovardhan566@gmail.com>
2023-11-12 23:41:49 -0700

    Removed Warnings

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
 import About from "./components/About";
-import Contact from "./components/Contact";
-import Projects from "./components/Projects";
 import NavProvider from "./context/NavContext";
 import Experience from "./components/Experience";
 
