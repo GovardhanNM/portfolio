ba5aa16be9b24089969b2aa70e460331a2bf8846
Govardhan Narasimha Murthy <ngovardhan566@gmail.com>
2023-11-12 23:39:43 -0700

    Removed Incomplete pages

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ function App() {
                 <Home />
                 <About />
                 <Experience />
-                <Projects />
-                <Contact />
             </NavProvider>
         </>
     );
